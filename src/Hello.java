c0d08ce3b524f78123c70d742086eec8d44c0d59
liuwei66 <2715084160@qq.com>
2022-09-18 18:23:08 +0800

    master1 commit1

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * @author by liuwei66
+ * @date : 2022-09-18 18:14
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("刘威是一个非常好学的人");
+    }
+}
