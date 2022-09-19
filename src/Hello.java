8ec7b79d0e2329892b32824d4c59fa48389f443e
liuwei66 <2715084160@qq.com>
2022-09-19 22:13:40 +0800

    commit7

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("刘威是一个非常好学的人");
-        System.out.println("刘威是一个很爱学习的人");
+        System.out.println("刘威是一个非常学习的人");
+        System.out.println("刘威是fechan善良的人");
     }
 }
